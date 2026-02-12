55d4c536998d15936a4b2e11d25073432e30eb7b
Erreur_504 <stefantroch@gmail.com>
2026-02-12 17:59:42 +0200

    Excecise 4 and 5 done

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,19 @@
-function greet(name: string) : string {
-    return `Salut c'est  ${name}!`
+import express,{Request,Response} from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/',(req : Request,res:Response) => {
+    res.send('Bienvenue sur mon serveur API');
+});
+app.listen(port,() =>{
+    console.log(`Serveur lancé sur http://localhost:${port}`);
+})
+
+
+/*function greet(name: string) : string {
+    return `Salut c'est  ${name}!`;
 }
 
 let message = greet('Johnny')
 
-console.log(message)
+console.log(message)*/
