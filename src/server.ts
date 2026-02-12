90fe47d6e510d1d349df3140133710c738bdb98d
Erreur_504 <stefantroch@gmail.com>
2026-02-12 18:26:27 +0200

    6.1 done

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,28 @@
 import express,{Request,Response} from 'express';
 const app = express();
 const port = 3000;
+const etudiants =[ 
+    {id : 1, nom : "Dupond", prenom : 'Jean'},
+    { id: 2, nom: "Martin", prenom: "Sophie" },
+    { id: 3, nom: "Doe", prenom: "John" },
+];
+
 
 app.get('/',(req : Request,res:Response) => {
     res.send('Bienvenue sur mon serveur API');
 });
+
+app.get('/api/data', (req : Request, res: Response) =>
+{res.json(etudiants);
+
+});
 app.listen(port,() =>{
     console.log(`Serveur lancé sur http://localhost:${port}`);
 })
+
+
+
+
 
 
 /*function greet(name: string) : string {
