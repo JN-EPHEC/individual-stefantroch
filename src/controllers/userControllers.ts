ad04f51b85189b6556a98b60895a864181204b95
Erreur_504 <stefantroch@gmail.com>
2026-03-02 19:14:08 +0100

    On avance : 2.4

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -3,12 +3,9 @@ import User from '../models/User';
 
 
 export const getAllUsers = async (req: Request, res: Response) => {
-try {
 const users = await User.findAll();
 res.status(200).json(users);
-} catch (error) {
-res.status(500).json({ error: (error as any).message });
-}
+
 };
 
 
@@ -16,35 +13,23 @@ export const postNewUser = async (req:Request,res : Response) =>{
     if (!req.body){
         res.status(400).json({error : 'body vide'})
     }
-
-    try {
         const newUSer = await User.create(req.body); // On recupere le body
         res.status(201).json({newUser : "utilisateur ajouté"}); // réponse coté client
-    } 
-    catch (error) {
-        res.status(500).json({ error: "Impossible de créer l'utilisateur" });
-    }
-
 };
 
 
 export const updatedUser = async (req : Request, res : Response) =>{
     const id = Number(req.params.id);        // ID = Number !!!! (sinon chaine txt)
-    try {
         const updatedUser = await User.update(
             req.body,   // données à mettre à jour
             { where: { id } }  // condition
         );
 
         res.status(200).json({ message: "Utilisateur mis à jour" });
-    } catch (error) {
-        res.status(500).json({ error: "Impossible de mettre à jour l'utilisateur" });
-    }
 };
 
 export const deleteUser = async (req :Request, res : Response) =>{
     const id = Number(req.params.id);        // ID = Number !!!! (sinon chaine txt)
-    try {
         const deleteUser = await User.destroy({
             where : {id} 
         });
@@ -53,10 +38,5 @@ export const deleteUser = async (req :Request, res : Response) =>{
         
         }
         res.status(200).json({message : "Utilisateur supprimé"});
-    }
-    catch(error){
-        res.status(500).json({error : 'Erreur du serveur'});
-    }
-
 };
 
