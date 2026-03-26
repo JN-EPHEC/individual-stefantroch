257e8a5a05b5dd7a968b8200269ac9ae811043fe
Erreur_504 <stefantroch@gmail.com>
2026-03-26 15:24:03 +0100

    wrf

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,9 +3,9 @@ function Header() {
     <div className="text-center mb-4">
       <h1>Base de données Users</h1>
       <p className="text-muted">
-        Ajout + suppression d’utilisateurs (React + API) ohsdblsdjklfd
+        Ajout + suppression d’utilisateurs (React + API)
       </p>
-      <p>ZLLLOOOOOOOOOxwcwcxwcOOO</p>
+      
     </div>
   );
 }
