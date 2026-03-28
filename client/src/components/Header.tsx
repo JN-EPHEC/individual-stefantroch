2522827a6b2583893efd8fd328a4b3870c6f3288
Erreur_504 <stefantroch@gmail.com>
2026-03-28 09:54:10 +0100

    on y est jprss

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,7 +3,7 @@ function Header() {
     <div className="text-center mb-4">
       <h1>Base de données Users</h1>
       <p className="text-muted">
-        Ajout + suppression d’utilisateurs (React + API) + clean du bordel précédent
+        Ajout + suppression d’utilisateurs (React + API) hola pendero
       </p>
       
     </div>
